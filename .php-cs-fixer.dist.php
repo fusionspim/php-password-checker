ea3275b3194cfeaad31e2b011d6367f3976c2410
Jamie York <ziadoz@gmail.com>
2022-02-15 12:34:04 +0000

    Remove Rector (#77)

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,7 +1,2 @@
 <?php
-$config = FusionsPim\PhpCsFixer\Factory::fromDefaults();
-
-return $config->setFinder(
-    $config->getFinder()
-        ->notName('rector.php')
-);
+return FusionsPim\PhpCsFixer\Factory::fromDefaults();
